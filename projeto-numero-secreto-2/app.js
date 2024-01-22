7c5f735db092d07a4eda2718b93728eec5a98380
JocaMotta1 <101892100+JocaMotta1@users.noreply.github.com>
2024-01-22 16:06:38 -0300

    versao inicial do jogo do numero secreto 2.0

---
+++ projeto-numero-secreto-2/app.js
@@ -0,0 +1,9 @@
+let title = document.querySelector('h1');
+title.innerHTML = "Você consegue adivinhar o número secreto?";
+
+let info = document.querySelector('p');
+info.innerHTML = "Escolha um número entre 1 e 100.";
+
+function verificarChute() {
+    
+}
