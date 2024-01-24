00c9d05d857337bb7c391400db03b096eb117271
JocaMotta1 <101892100+JocaMotta1@users.noreply.github.com>
2024-01-24 15:49:46 -0300

    logica de reiniciar o jogo feita
    .

--- projeto-numero-secreto-2/app.js
+++ projeto-numero-secreto-2/app.js
@@ -1,9 +1,4 @@
-let numeroSecreto = gerarNumeroAleatorio();
-let tentativas = 0;
-
-selecionarTag('h1', "Você consegue adivinhar o número secreto?");
-selecionarTag('p', "Escolha um número entre 1 e 100. (apenas números inteiros)\nTentativas: " + tentativas);
-
+iniciarJogo();
 verificarChute();
 
 function verificarChute() {
@@ -11,6 +6,7 @@ function verificarChute() {
         tentativas++;
         let stringTentativa = tentativas > 1 ? "tentativas" : "tentativa";
         selecionarTag('p', "Parabéns! Você conseguiu acertar o número misterioso após " + tentativas + " " + stringTentativa + ".\n\nO número era o " + numeroSecreto + '.');
+        document.querySelector("button#iniciar").removeAttribute("disabled");
     }
     else {
         if (document.querySelector("input").value > numeroSecreto) {
@@ -21,7 +17,16 @@ function verificarChute() {
             tentativas++;
             selecionarTag('p', "O seu número é MENOR do que o número misterioso. Tente novamente!\nTentativas: " + tentativas);
         }
+        document.querySelector("input").value = "";
     }
+}
+
+function iniciarJogo() {
+    numeroSecreto = Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1) + 1)) + Math.ceil(1);
+    tentativas = 0;
+    exibirMensagem();
+    document.querySelector("input").value = "";
+    document.querySelector("button#iniciar").setAttribute("disabled", true);
 }
 
 function selecionarTag (tag, texto) {
@@ -29,6 +34,7 @@ function selecionarTag (tag, texto) {
     field.innerHTML = texto;
 }
 
-function gerarNumeroAleatorio() {
-    return Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1) + 1)) + Math.ceil(1);
+function exibirMensagem() {
+    selecionarTag('h1', "Você consegue adivinhar o número secreto?");
+    selecionarTag('p', "Escolha um número entre 1 e 100. (apenas números inteiros)\nTentativas: " + tentativas);
 }
