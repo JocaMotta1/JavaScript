d282c1d8d8bfd8c0543e75a790725d2990a637b3
JocaMotta1 <101892100+JocaMotta1@users.noreply.github.com>
2024-01-25 17:03:09 -0300

    joguinho acho que 99% finalizado :)
    .

--- projeto-numero-secreto-2/app.js
+++ projeto-numero-secreto-2/app.js
@@ -1,3 +1,5 @@
+let numerosSorteados = [];
+
 iniciarJogo();
 verificarChute();
 
@@ -5,17 +7,18 @@ function verificarChute() {
     if (document.querySelector("input").value == numeroSecreto) {
         tentativas++;
         let stringTentativa = tentativas > 1 ? "tentativas" : "tentativa";
-        selecionarTag('p', "Parabéns! Você conseguiu acertar o número misterioso após " + tentativas + " " + stringTentativa + ".\n\nO número era o " + numeroSecreto + '.');
+        selecionarTag('p', "Parabéns! Você conseguiu acertar o número misterioso.");
+        document.querySelector("button#chutar").setAttribute("disabled", true);
         document.querySelector("button#iniciar").removeAttribute("disabled");
     }
     else {
         if (document.querySelector("input").value > numeroSecreto) {
             tentativas++;
-            selecionarTag('p', "O seu número é MAIOR do que o número misterioso. Tente novamente!\nTentativas: " + tentativas);
+            selecionarTag('p', "O seu número é MAIOR do que o número misterioso!");
         }
         else if (document.querySelector("input").value != "" && document.querySelector("input").value < numeroSecreto) {
             tentativas++;
-            selecionarTag('p', "O seu número é MENOR do que o número misterioso. Tente novamente!\nTentativas: " + tentativas);
+            selecionarTag('p', "O seu número é MENOR do que o número misterioso!");
         }
         document.querySelector("input").value = "";
     }
@@ -23,10 +26,21 @@ function verificarChute() {
 
 function iniciarJogo() {
     numeroSecreto = Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1) + 1)) + Math.ceil(1);
-    tentativas = 0;
-    exibirMensagem();
-    document.querySelector("input").value = "";
-    document.querySelector("button#iniciar").setAttribute("disabled", true);
+    if (numerosSorteados.length == 100) {
+        numerosSorteados = [];
+        selecionarTag('p', "Todos os números possíveis já foram sorteados. Clique para jogar novamente e gerar uma nova sequência!");
+    }
+    else {
+        while (numerosSorteados.includes(numeroSecreto)) {
+            numeroSecreto = Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1) + 1)) + Math.ceil(1);
+        }
+        numerosSorteados.push(numeroSecreto);
+        tentativas = 0;
+        exibirMensagem();
+        document.querySelector("input").value = "";
+        document.querySelector("button#chutar").removeAttribute("disabled");
+        document.querySelector("button#iniciar").setAttribute("disabled", true);
+    }
 }
 
 function selecionarTag (tag, texto) {
@@ -36,5 +50,5 @@ function selecionarTag (tag, texto) {
 
 function exibirMensagem() {
     selecionarTag('h1', "Você consegue adivinhar o número secreto?");
-    selecionarTag('p', "Escolha um número entre 1 e 100. (apenas números inteiros)\nTentativas: " + tentativas);
+    selecionarTag('p', "Escolha um número entre 1 e 100.");
 }
